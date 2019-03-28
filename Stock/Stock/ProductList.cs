f58a62007a15a2cc7db640608189cefb2d2e25ea
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-28 11:33:10 +0600

    Добавление файлов проекта

---
+++ Stock/Stock/ProductList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Stock
+{
+    public delegate void Add(string text);
+    public class ProductList<T>:IRepository<T>
+    {
+        public event Add ProductAdded;
+
+        public void Add(T entity)
+        {
+            ProductAdded($"Товар добавлен");
+            var data = GetAll();
+            data.Add(entity);
+            using (StreamWriter stream = File.CreateText("productlist.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(stream, data);
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            ProductAdded($"Товар удален");
+            var data = GetAll();
+            data.Remove(entity);
+            using (StreamWriter stream = File.CreateText("productlist.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(stream, data);
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            try
+            {
+                using (StreamReader stream = File.OpenText("productlist.json"))
+                {
+                    if (stream.Peek() < 0)
+                    {
+                        return new List<T>();
+                    }
+                    JsonSerializer serializer = new JsonSerializer();
+                    return (List<T>)serializer.Deserialize(stream, typeof(List<T>));
+                }                
+            }
+            catch (FileNotFoundException)
+            {
+                File.CreateText("productlist.json").Close();
+                return new List<T>();
+            }
+        }
+    }
+}
---
+++ Stock/Stock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductList<Product> productList = new ProductList<Product>();
+            productList.ProductAdded += ConsoleMessage;
+            productList.Add(new Product() { Name = "Coca-Cola", Description = "carbonated drink" });
+
+
+            Console.Read();
+        }
+
+        public static void ConsoleMessage(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
